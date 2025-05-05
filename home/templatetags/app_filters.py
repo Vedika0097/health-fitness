dbebac2d8f3116fc7d1ad2afc035d3a3806a7447
unknown <vedika@bennett.edu.in>
2025-05-05 09:46:16 +0530

    dashboard changes

--- home/templatetags/app_filters.py
+++ home/templatetags/app_filters.py
@@ -7,4 +7,5 @@ register = template.Library()
 def calcnutperc(qty, targetQty):
     actual = float(qty)
     target = float(targetQty)
-    return math.ceil(((actual/target) * 100))
+    val = round(math.ceil(((actual/target) * 100))/5)*5
+    return val
